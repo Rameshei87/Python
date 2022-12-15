0ac0e08915bf0c0f84ee690d4dbcc567c7d276b9
Ramesh chinnaraj <110036451+Rameshei87@users.noreply.github.com>
2022-12-16 08:16:22 +1000

    Add files via upload

---
+++ bts_voice_report_region_MTD.sql
@@ -0,0 +1,33 @@
+\o /home/ramesh/Automation/reports/SI_bts_mou_voice_Region_MTD.csv
+select REGION,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-01' then actual_usage end )/60) JAN_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-02' then actual_usage end )/60) FEB_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-03' then actual_usage end )/60) MAR_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-04' then actual_usage end )/60) APR_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-05' then actual_usage end )/60) MAY_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-06' then actual_usage end )/60) JUN_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-07' then actual_usage end )/60) JUL_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-08' then actual_usage end )/60) AUG_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-09' then actual_usage end )/60) SEP_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-10' then actual_usage end )/60) OCT_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-11' then actual_usage end )/60) NOV_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-12' then actual_usage end )/60) DEC_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2020-12' then actual_usage end )/60) DEC_2020_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-01' then actual_usage end )/60) JAN_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-02' then actual_usage end )/60) FEB_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-03' then actual_usage end )/60) MAR_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-04' then actual_usage end )/60) APR_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-05' then actual_usage end )/60) MAY_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-06' then actual_usage end )/60) JUN_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-07' then actual_usage end )/60) JUL_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-08' then actual_usage end )/60) AUG_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-09' then actual_usage end )/60) SEP_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-10' then actual_usage end )/60) OCT_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-11' then actual_usage end )/60) NOV_2021_MOU,
+round(sum( case when to_char(CUST_LOCAL_END_DATE,'YYYY-MM') = '2021-12' then actual_usage end )/60) DEC_2021_MOU
+from report.dw_cell_id_2017_v2 a,cbs_cdr_voice b
+where a.cell_id = substr(b.Calling_Cell_ID,11,5)
+and a.lac = substr(b.Calling_Cell_ID,6,5)
+and CUST_LOCAL_END_DATE >= '01-Jan-2020'
+group by REGION
+order by REGION;
